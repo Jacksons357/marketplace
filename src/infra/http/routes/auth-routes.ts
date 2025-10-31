2cb5873a75fb35fcd93a286a87101719c8470cad
Jackson Trexx <jackson@trexx.com.br>
2025-10-31 02:21:39 -0300

    chore: remove /register in auth routes

--- src/infra/http/routes/auth-routes.ts
+++ src/infra/http/routes/auth-routes.ts
@@ -20,7 +20,7 @@ export function authRoutes(app: FastifyInstance) {
   const adminController = makeAdminController()
   const authController = makeAuthController()
 
-  app.post("/admin/register", {
+  app.post("/admin", {
     schema: {
       tags: ['Auth'],
       summary: 'Register a new admin',
@@ -33,7 +33,7 @@ export function authRoutes(app: FastifyInstance) {
     ]
   }, async (req: FastifyRequest, res: FastifyReply) => adminController.register(req, res));
 
-  app.post('/user/register', {
+  app.post('/user', {
     schema: {
       tags: ['Auth'],
       summary: 'Register a new user',

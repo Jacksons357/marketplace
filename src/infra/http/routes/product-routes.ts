5b51f2c02b4cadae9e6f4489b02159c53759e6dc
Jackson Trexx <jackson@trexx.com.br>
2025-11-01 14:08:02 -0300

    feat: add swagger for product routes

--- src/infra/http/routes/product-routes.ts
+++ src/infra/http/routes/product-routes.ts
@@ -1,11 +1,27 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { makeProductListController } from "../../factories/make-product-list";
 import { makeProductAiSearchController } from "../../factories/make-product-ai-search";
+import { productListQuerySchemaDocs } from "../../../presentation/docs/swagger";
 
 export function productRoutes(app: FastifyInstance) {
   const productListController = makeProductListController()
   const productAiSearchController = makeProductAiSearchController()
 
-  app.get('/', async (req: FastifyRequest, res: FastifyReply) => productListController.list(req, res))
-  app.get('/ai', async (req: FastifyRequest, res: FastifyReply) => productAiSearchController.search(req, res))
+  app.get('/', {
+    schema: {
+      tags: ['Products'],
+      summary: 'List Products',
+      description: 'List all products of the all organization and filter by category, price range, and search term and pagination',
+      querystring: productListQuerySchemaDocs
+    }
+  }, async (req: FastifyRequest, res: FastifyReply) => productListController.list(req, res))
+
+  app.get('/ai', {
+    schema: {
+      tags: ['Products'],
+      summary: 'AI Search Products',
+      description: 'Search products using AI and filter by category, price range, and search term and pagination',
+      querystring: productListQuerySchemaDocs
+    }
+  }, async (req: FastifyRequest, res: FastifyReply) => productAiSearchController.search(req, res))
 }

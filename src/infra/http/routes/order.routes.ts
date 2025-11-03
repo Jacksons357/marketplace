1dc0f805f1eb874b9c5b039157f03d064e670c0b
Jackson Trexx <jackson@trexx.com.br>
2025-11-02 21:55:12 -0300

    docs: add swagger schema for order route

--- src/infra/http/routes/order.routes.ts
+++ src/infra/http/routes/order.routes.ts
@@ -3,12 +3,26 @@ import { makeOrderController } from "../../factories/make-order";
 import validateBody from "../middlewares/validate-body";
 import { orderCreateSchema } from "../../../application/dtos/OrderCreateDTO";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { 
+  createOrderBodySchemaDocs, 
+  createOrderResponseSchemaDocs, 
+  headersSchemaDocs 
+} from "../../../presentation/docs/swagger";
 
 export const orderRoutes = async (app: FastifyInstance) => {
   app.addHook('preHandler', authMiddleware)
   const orderController = makeOrderController()
 
   app.post('/', {
+    schema: {
+      tags: ['Orders'],
+      summary: 'Create a new order',
+      description: 'Create a new order in the marketplace with organization and product details',
+      security: [{ BearerAuth: []}],
+      headers: headersSchemaDocs,
+      body: createOrderBodySchemaDocs,
+      response: createOrderResponseSchemaDocs,
+    },
     preHandler: [
       validateBody(orderCreateSchema),
     ]
--- src/presentation/docs/swagger/index.ts
+++ src/presentation/docs/swagger/index.ts
@@ -1,3 +1,4 @@
 export * from './auth/adminSchemaDocs'
 export * from './auth/userSchemaDocs'
 export * from './admin/productSchemaDocs'
+export * from './order/orderSchemaDocs'

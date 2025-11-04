c3ea271f4a39108d3dcc7e63e72a4f31340ec576
Jackson Trexx <jackson@trexx.com.br>
2025-11-04 03:55:02 -0300

    feat(backend): update schema response docs in order route

--- backend/src/presentation/docs/swagger/order/orderSchemaDocs.ts
+++ backend/src/presentation/docs/swagger/order/orderSchemaDocs.ts
@@ -51,3 +51,39 @@ export const createOrderResponseSchemaDocs = {
   }
 }
 
+export const getOrdersResponseSchemaDocs = {
+  200: {
+    description: 'OK',
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        id: { type: 'string', format: 'uuid' },
+        userId: { type: 'string', format: 'uuid' },
+        organizationId: { type: 'string', format: 'uuid' },
+        totalPrice: { type: 'number' },
+        status: { type: 'string' },
+        createdAt: { type: 'string', format: 'date-time' },
+        updatedAt: { type: 'string', format: 'date-time' },
+        items: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'string', format: 'uuid' },
+              orderId: { type: 'string', format: 'uuid' },
+              productId: { type: 'string', format: 'uuid' },
+              organizationId: { type: 'string', format: 'uuid' },
+              quantity: { type: 'number' },
+              priceAtPurchase: { type: 'number' },
+              createdAt: { type: 'string', format: 'date-time' },
+              updatedAt: { type: 'string', format: 'date-time' },
+              name: { type: 'string' },
+            },
+          }
+        },
+        organizationName: { type: 'string' },
+      },
+    }
+  }
+}
